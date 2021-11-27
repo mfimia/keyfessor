b313021b0158c553d817b8726c62bd9d075643b6
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-27 12:22:43 +0100

    added keyboard basic layout �

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,3 +1,28 @@
+import "../css/Keyboard.css";
 export default function Keyboard() {
-  return <p>Keyboard display</p>;
+  const topRow = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const topRowKeys = topRow.map((key, index) => (
+    <span className="keyboard--key" key={index}>
+      {key}
+    </span>
+  ));
+  const midRow = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const midRowKeys = midRow.map((key, index) => (
+    <span className="keyboard--key" key={index}>
+      {key}
+    </span>
+  ));
+  const botRow = ["Z", "X", "C", "V", "B", "N", "M"];
+  const botRowKeys = botRow.map((key, index) => (
+    <span className="keyboard--key" key={index}>
+      {key}
+    </span>
+  ));
+  return (
+    <div className="keyboard--container">
+      <div className="keyboard--top--row">{topRowKeys}</div>
+      <div className="keyboard--mid--row">{midRowKeys}</div>
+      <div className="keyboard--bot--row">{botRowKeys}</div>
+    </div>
+  );
 }
