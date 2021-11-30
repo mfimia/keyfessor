15f40121c5be632afd6b40401e4a1ada048fcf23
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-30 09:32:46 +0100

    fixed text length

--- src/components/Main.js
+++ src/components/Main.js
@@ -52,10 +52,10 @@ export default function Main() {
 
   const advanceText = () => {
     setCurrentLetter((prev) => {
-      return prev === 5 ? newText() : prev + 1;
+      return prev === lettersArray.totalLetters - 1 ? newText() : prev + 1;
     });
   };
-  // lettersArray.totalLetters - 1
+
   const newText = () => {
     if (displayedText.currentText === textArray.length - 1) {
       endGame.current = true;
