afbc1e5ac05bcd42f180dab8d3353aeeb35619ec
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-01 09:40:51 +0100

    added text at start to improve usability

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,38 @@
 import "../css/Navbar.css";
-// import PlayButton from "./PlayButton";
 import lightLogo from "../assets/keyfessor-white-logo.png";
 import darkLogo from "../assets/keyfessor-black-logo.png";
+import useEventListener from "@use-it/event-listener";
+import Pointer from "./Pointer";
+import { useState } from "react";
 
 export default function Navbar({ darkMode }) {
+  const [onLoad, setOnLoad] = useState(true);
+
+  const startApp = ({ key }) => {
+    if (key === "m" && onLoad) {
+      setOnLoad(false);
+    }
+  };
+
+  useEventListener("keydown", startApp);
+
   return (
-    <img
-      alt="Keyfessor logo"
-      className="header--logo"
-      src={darkMode ? lightLogo : darkLogo}
-    />
+    <>
+      <img
+        alt="Keyfessor logo"
+        className="header--logo"
+        src={darkMode ? lightLogo : darkLogo}
+      />
+      {onLoad && (
+        <div className={darkMode ? "start--typing--dark" : "start--typing"}>
+          Press{" "}
+          <span>
+            "m"
+            <Pointer darkMode={darkMode} />
+          </span>{" "}
+          to start
+        </div>
+      )}
+    </>
   );
 }
