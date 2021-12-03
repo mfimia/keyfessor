798a2cfdfe90a31db3fbf78a111125353b06c308
Tamara Ilic <tamara@ilicmcfarlane.io>
2021-12-03 21:41:24 +0000

    refactor Keyboard.js

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,13 +1,13 @@
-import "../css/Keyboard.css";
-import useEventListener from "@use-it/event-listener";
-import keys from "./keys";
-import keyChecker from "./keyChecker";
-import sound from "../assets/Mechanical-Keyboard-single-button-presses-4.mp3";
-import error from "../assets/mixkit-electric-fence-buzzing-2967.wav";
-const keySound = new Audio(sound);
-const errorSound = new Audio(error);
-errorSound.volume = 0.15;
-keySound.volume = 0.5;
+import '../css/Keyboard.css'
+import useEventListener from '@use-it/event-listener'
+import keys from './keys'
+import keyChecker from './keyChecker'
+import sound from '../assets/Mechanical-Keyboard-single-button-presses-4.mp3'
+import error from '../assets/mixkit-electric-fence-buzzing-2967.wav'
+const keySound = new Audio(sound)
+const errorSound = new Audio(error)
+errorSound.volume = 0.15
+keySound.volume = 0.5
 
 export default function Keyboard({
   lettersArray,
@@ -15,41 +15,43 @@ export default function Keyboard({
   currentLetter,
   addError,
   handleStartTimer,
-  isRunning,
+  isRunning
 }) {
   function highlightPressedKey({ key }) {
-    const pressedLetter = key === " " ? "spacebar" : key;
-    const activeKey = document.getElementById(pressedLetter);
+    const pressedLetter = key === ' ' ? 'spacebar' : key
+    const activeKey = document.getElementById(pressedLetter)
     if (activeKey) {
       if (keyChecker(lettersArray, currentLetter, pressedLetter)) {
-        handleStartTimer();
-        keySound.currentTime = 0.12;
-        keySound.play();
-        activeKey.classList.add("active-key__correct");
-        advanceText();
-      } else if (
+        handleStartTimer()
+        keySound.currentTime = 0.12
+        keySound.play()
+        activeKey.classList.add('active-key__correct')
+        advanceText()
+      }
+
+      if (
         !keyChecker(lettersArray, currentLetter, pressedLetter) &&
         isRunning
       ) {
-        errorSound.currentTime = 0.12;
-        errorSound.play();
-        activeKey.classList.add("active-key__wrong");
-        addError();
-      } else {
-        return;
+        errorSound.currentTime = 0.12
+        errorSound.play()
+        activeKey.classList.add('active-key__wrong')
+        addError()
       }
+
+      return
     }
   }
 
   function removeHighlightFromPressedKey({ key }) {
-    const pressedLetter = key === " " ? "spacebar" : key;
-    const activeKey = document.getElementById(pressedLetter);
+    const pressedLetter = key === ' ' ? 'spacebar' : key
+    const activeKey = document.getElementById(pressedLetter)
     if (activeKey) {
-      activeKey.classList.remove("active-key__correct") ||
-        activeKey.classList.remove("active-key__wrong");
-    } else {
-      return;
+      activeKey.classList.remove('active-key__correct') ||
+        activeKey.classList.remove('active-key__wrong')
     }
+
+    return
   }
 
   const keyboard = keys.map((row, index) => {
@@ -59,19 +61,19 @@ export default function Keyboard({
           return (
             <span
               id={rowKey.toLowerCase()}
-              className={rowKey === "Spacebar" ? "spacebar" : "keyboard--key"}
+              className={rowKey === 'Spacebar' ? 'spacebar' : 'keyboard--key'}
               key={keyIndex}
             >
-              {rowKey === "Spacebar" ? "-" : rowKey}
+              {rowKey === 'Spacebar' ? '-' : rowKey}
             </span>
-          );
+          )
         })}
       </div>
-    );
-  });
+    )
+  })
 
-  useEventListener("keydown", highlightPressedKey);
-  useEventListener("keyup", removeHighlightFromPressedKey);
+  useEventListener('keydown', highlightPressedKey)
+  useEventListener('keyup', removeHighlightFromPressedKey)
 
-  return <div className="keyboard--container">{keyboard}</div>;
+  return <div className='keyboard--container'>{keyboard}</div>
 }
