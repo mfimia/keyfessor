159301c94c78aee3679fb7f43c61a5137ccbd67c
Tamara Ilic <tamara@ilicmcfarlane.io>
2021-12-03 21:53:18 +0000

    convert keyboard to lower case keys

--- src/components/keys.js
+++ src/components/keys.js
@@ -1,20 +1,20 @@
 const keys = [
   {
-    keys: ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-    className: "keyboard--top--row",
+    keys: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    className: 'keyboard--top--row'
   },
   {
-    keys: ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-    className: "keyboard--mid--row",
+    keys: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    className: 'keyboard--mid--row'
   },
   {
-    keys: ["Z", "X", "C", "V", "B", "N", "M", ",", "."],
-    className: "keyboard--bot--row",
+    keys: ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.'],
+    className: 'keyboard--bot--row'
   },
   {
-    keys: ["Spacebar"],
-    className: "spacebar--row",
-  },
-];
+    keys: ['Spacebar'],
+    className: 'spacebar--row'
+  }
+]
 
-export default keys;
+export default keys
