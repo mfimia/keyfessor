ef02222a8cd4e103ef9d9377482a7c7d8d587aee
Tamara Ilic <tamara@ilicmcfarlane.io>
2021-12-03 21:34:33 +0000

    refactor keyChecker

--- src/components/keyChecker.js
+++ src/components/keyChecker.js
@@ -1,20 +1,17 @@
 export default function keyChecker(lettersArray, currentLetter, pressedLetter) {
   if (
-    pressedLetter === "spacebar" &&
-    lettersArray.remainingLetters[currentLetter] === " "
+    pressedLetter === 'spacebar' &&
+    lettersArray.remainingLetters[currentLetter] === ' '
   ) {
-    return true;
-  } else if (
-    pressedLetter === "spacebar" &&
-    lettersArray.remainingLetters[currentLetter] !== " "
-  ) {
-    return false;
-  } else if (
-    pressedLetter !== "spacebar" &&
+    return true
+  }
+
+  if (
+    pressedLetter !== 'spacebar' &&
     pressedLetter === lettersArray.remainingLetters[currentLetter]
   ) {
-    return true;
-  } else {
-    return false;
+    return true
   }
+
+  return false
 }
