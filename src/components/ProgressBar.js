0e355c10a2034e32efb76e105cb1d91e44ce2734
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-04 10:52:01 +0100

    added basic styling to progess bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,6 +1,6 @@
-import '../css/ProgressBar.css'
+import "../css/ProgressBar.css";
 
 export default function ProgressBar(props) {
-  const { value, max } = props
-  return <progress value={value} max={max} />
+  const { value, max } = props;
+  return <progress className="text--progress" value={value} max={max} />;
 }
