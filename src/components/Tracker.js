2d60e5034f9433220b7155de9b1c9cb43d6f0bf8
Tamara Ilic <tamara@ilicmcfarlane.io>
2021-12-04 00:00:59 +0000

    fix accuracy bugs and prevent negative accuracy

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,26 +1,26 @@
-import "../css/Tracker.css";
+import '../css/Tracker.css'
 export default function Tracker({
   lettersArray,
   currentLetter,
   wordsPerMinute,
-  accuracy,
+  accuracy
 }) {
   return (
-    <div className="tracker--panel--container">
-      <div className="speed--panel">
+    <div className='tracker--panel--container'>
+      <div className='speed--panel'>
         Typed: <br />
-        <span className="panel--metric">
+        <span className='panel--metric'>
           {currentLetter}/{lettersArray.totalLetters}
         </span>
       </div>
-      <div className="speed--panel">
+      <div className='speed--panel'>
         Speed: <br />
-        <span className="panel--metric">{wordsPerMinute} WPM</span>
+        <span className='panel--metric'>{wordsPerMinute} WPM</span>
       </div>
-      <div className="accuracy--panel">
+      <div className='accuracy--panel'>
         Accuracy: <br />
-        <span className="panel--metric">{accuracy || 100}%</span>
+        <span className='panel--metric'>{accuracy}%</span>
       </div>
     </div>
-  );
+  )
 }
