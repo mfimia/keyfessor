7c507149669e292aefe8ef6dbbd6903dc92d937d
Tamara Ilic <tamara@ilicmcfarlane.io>
2021-12-04 00:13:09 +0000

    make start instruction dynamic based on the text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,58 @@
-import "../css/Navbar.css";
-import lightLogo from "../assets/keyfessor-white-logo.png";
-import darkLogo from "../assets/keyfessor-black-logo.png";
-import useEventListener from "@use-it/event-listener";
-import Pointer from "./Pointer";
-import { useState } from "react";
+import '../css/Navbar.css'
+import lightLogo from '../assets/keyfessor-white-logo.png'
+import darkLogo from '../assets/keyfessor-black-logo.png'
+import useEventListener from '@use-it/event-listener'
+import Pointer from './Pointer'
+import { useState } from 'react'
+import textArray from './textData'
+import Text from './Text'
 
 export default function Navbar({ darkMode }) {
-  const [onLoad, setOnLoad] = useState(true);
+  const [onLoad, setOnLoad] = useState(true)
+
+  // TODO: refactor this to avoid importing all this just to retrieve one letter
+  const [displayedText, setDisplayedText] = useState({
+    texts: textArray,
+    currentText: 0
+  })
+  const [lettersArray, setLettersArray] = useState({
+    totalLetters:
+      displayedText.texts[displayedText.currentText].split('').length,
+    remainingLetters: displayedText.texts[displayedText.currentText].split(''),
+    completedLetters: [],
+    errors: 0
+  })
+
+  const firstLetter = displayedText.texts[displayedText.currentText][0]
 
   const startApp = ({ key }) => {
-    if (key === "m" && onLoad) {
-      setOnLoad(false);
+    if (key === firstLetter && onLoad) {
+      setOnLoad(false)
     }
-  };
+  }
 
-  useEventListener("keydown", startApp);
+  useEventListener('keydown', startApp)
 
   return (
     <>
       <img
-        alt="Keyfessor logo"
-        className="header--logo"
+        alt='Keyfessor logo'
+        className='header--logo'
         src={darkMode ? lightLogo : darkLogo}
       />
       {onLoad && (
         <div
-          id="start--text"
-          className={darkMode ? "start--typing--dark" : "start--typing"}
+          id='start--text'
+          className={darkMode ? 'start--typing--dark' : 'start--typing'}
         >
-          Type{" "}
+          Type{' '}
           <span>
-            "m"
+            {firstLetter}
             <Pointer darkMode={darkMode} />
-          </span>{" "}
+          </span>{' '}
           to start
         </div>
       )}
     </>
-  );
+  )
 }
