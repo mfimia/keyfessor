dacd93796dcbc62a56207d1257915812a6fe6ac6
Fimia Osuna <martin.fimia.osuna@hotmail.com>
2021-12-22 19:10:53 +0100

    added fragment

--- src/components/layout/StartScreen.js
+++ src/components/layout/StartScreen.js
@@ -1,5 +1,6 @@
+import { Fragment } from "react";
 import ".././css/StartScreen.css";
 
 export default function StartScreen(props) {
-  return <>{props.children}</>;
+  return <Fragment>{props.children}</Fragment>;
 }
