f4b5a43816e2d04636855cb1fb4f76e1f979ae74
Fimia Osuna <martin.fimia.osuna@hotmail.com>
2021-12-22 19:10:10 +0100

    refactored to import state from main state

--- src/components/layout/Tracker.js
+++ src/components/layout/Tracker.js
@@ -1,26 +1,27 @@
-import '../css/Tracker.css'
-export default function Tracker({
-  lettersArray,
-  currentLetter,
-  wordsPerMinute,
-  accuracy
-}) {
+import { useContext } from "react";
+import MainContext from "../../context/main/MainContext";
+import "../css/Tracker.css";
+
+export default function Tracker() {
+  const mainContext = useContext(MainContext);
+  const { lettersArray, currentLetter, wordsPerMinute, accuracy } = mainContext;
+
   return (
-    <div className='tracker--panel--container'>
-      <div className='speed--panel'>
+    <div className="tracker--panel--container">
+      <div className="speed--panel">
         Typed: <br />
-        <span className='panel--metric'>
+        <span className="panel--metric">
           {currentLetter}/{lettersArray.totalLetters}
         </span>
       </div>
-      <div className='speed--panel'>
+      <div className="speed--panel">
         Speed: <br />
-        <span className='panel--metric'>{wordsPerMinute} WPM</span>
+        <span className="panel--metric">{wordsPerMinute} WPM</span>
       </div>
-      <div className='accuracy--panel'>
+      <div className="accuracy--panel">
         Accuracy: <br />
-        <span className='panel--metric'>{accuracy}%</span>
+        <span className="panel--metric">{accuracy}%</span>
       </div>
     </div>
-  )
+  );
 }
