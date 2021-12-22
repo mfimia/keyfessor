add9c6e269f6b00c7eb6fe7db13966a72005c3e5
Fimia Osuna <martin.fimia.osuna@hotmail.com>
2021-12-22 18:59:06 +0100

    removed component level state from main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,120 +8,7 @@ import ProgressBar from "./layout/ProgressBar";
 import EndScreen from "./layout/EndScreen";
 import Pointer from "./layout/Pointer";
 
-export default function Main(props) {
-  const [currentLetter, setCurrentLetter] = useState(0);
-  const [displayedText, setDisplayedText] = useState({
-    texts: textArray,
-    currentText: 0,
-  });
-  const [lettersArray, setLettersArray] = useState({
-    totalLetters:
-      displayedText.texts[displayedText.currentText].split("").length,
-    remainingLetters: displayedText.texts[displayedText.currentText].split(""),
-    completedLetters: [],
-    errors: 0,
-  });
-  const restarted = useRef(false);
-  const endGame = useRef(false);
-
-  const firstLetter = displayedText.texts[displayedText.currentText][0];
-
-  const {
-    isRunning,
-    addLap,
-    stopTimer,
-    laps,
-    elapsedTime,
-    startTimer,
-    resetTimer,
-    resetLaps,
-  } = useStopwatch();
-
-  const averageLengthWord =
-    lettersArray.totalLetters /
-    lettersArray.remainingLetters.join("").split(" ").length;
-  const minutes = elapsedTime / 60;
-  const wordsPerMinute =
-    Math.floor(currentLetter / averageLengthWord / minutes) || 0;
-
-  let accuracy = 100;
-  if (isRunning) {
-    accuracy =
-      lettersArray.errors > currentLetter
-        ? 0
-        : Math.floor((1 - lettersArray.errors / currentLetter) * 100);
-  }
-
-  const handleStartTimer = () => {
-    if (!isRunning) startTimer();
-  };
-
-  const advanceText = () => {
-    setCurrentLetter((prev) => {
-      return prev === lettersArray.totalLetters - 1 ? newText() : prev + 1;
-    });
-  };
-
-  const newText = () => {
-    if (displayedText.currentText === textArray.length - 1) {
-      endGame.current = true;
-    } else {
-      setDisplayedText((previousText) => {
-        return {
-          ...previousText,
-          currentText: previousText.currentText + 1,
-        };
-      });
-
-      if (!isRunning) stopTimer();
-      addLap(wordsPerMinute, accuracy);
-      resetTimer();
-      setCurrentLetter(0);
-    }
-  };
-
-  useEffect(() => {
-    if (displayedText.currentText || restarted.current) {
-      restarted.current = false;
-      setLettersArray((prev) => {
-        return {
-          ...prev,
-          totalLetters:
-            displayedText.texts[displayedText.currentText].split("").length,
-          remainingLetters:
-            displayedText.texts[displayedText.currentText].split(""),
-          errors: 0,
-        };
-      });
-    }
-  }, [displayedText.texts, displayedText.currentText]);
-
-  let progressBarMaxValue =
-    displayedText.texts[displayedText.currentText].split("").length;
-
-  const addError = () => {
-    setLettersArray((prev) => {
-      return {
-        ...prev,
-        errors: prev.errors + 1,
-      };
-    });
-  };
-
-  const resetGame = () => {
-    restarted.current = true;
-    setCurrentLetter(0);
-    setDisplayedText((prevText) => {
-      return {
-        ...prevText,
-        currentText: 0,
-      };
-    });
-    resetTimer();
-    resetLaps();
-    endGame.current = false;
-  };
-
+export default function Main() {
   return (
     <>
       {!isRunning ? (
@@ -140,28 +27,10 @@ export default function Main(props) {
         ""
       )}
 
-      <Text
-        currentLetter={currentLetter}
-        displayedText={displayedText.texts[displayedText.currentText]}
-      />
-      <ProgressBar
-        value={(currentLetter / progressBarMaxValue) * progressBarMaxValue}
-        max={progressBarMaxValue}
-      />
-      <TypingPanel
-        displayedText={displayedText.texts[displayedText.currentText]}
-        lettersArray={lettersArray}
-        advanceText={advanceText}
-        currentLetter={currentLetter}
-        addError={addError}
-        handleStartTimer={handleStartTimer}
-        elapsedTime={elapsedTime}
-        laps={laps}
-        wordsPerMinute={wordsPerMinute}
-        accuracy={accuracy}
-        isRunning={isRunning}
-      />
-      {endGame.current && <EndScreen laps={laps} resetGame={resetGame} />}
+      <Text />
+      <ProgressBar />
+      <TypingPanel />
+      {endGame.current && <EndScreen />}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import MainState from "./context/main/MainState";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <MainState>
+    <App />
+  </MainState>,
+  document.getElementById("root")
+);
