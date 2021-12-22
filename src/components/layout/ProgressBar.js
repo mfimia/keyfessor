b02eb91d6f7f95a839974d37f0bc75e9be215176
Fimia Osuna <martin.fimia.osuna@hotmail.com>
2021-12-22 19:12:30 +0100

    refactored to import state from main state

--- src/components/layout/ProgressBar.js
+++ src/components/layout/ProgressBar.js
@@ -1,6 +1,11 @@
+import { useContext } from "react";
+import MainContext from "../../context/main/MainContext";
 import "../css/ProgressBar.css";
 
 export default function ProgressBar() {
+  const mainContext = useContext(MainContext);
+  const { displayedText, currentLetter } = mainContext;
+
   const progressBarMaxValue =
     displayedText.texts[displayedText.currentText].split("").length;
   const value = (currentLetter / progressBarMaxValue) * progressBarMaxValue;
