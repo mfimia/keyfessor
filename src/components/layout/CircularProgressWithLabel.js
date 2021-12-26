297e3e2142e086bfc0678bd1da4ba3775bb1075f
Fimia Osuna <martin.fimia.osuna@hotmail.com>
2021-12-26 08:14:26 +0100

    aligned tracker progress to right side

--- src/components/layout/CircularProgressWithLabel.js
+++ src/components/layout/CircularProgressWithLabel.js
@@ -4,7 +4,14 @@ import Box from "@mui/material/Box";
 
 const CircularProgressWithLabel = ({ value, unit, percentage, title }) => {
   return (
-    <Box>
+    <Box
+      sx={{
+        border: 1,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-end",
+      }}
+    >
       <Typography component="h5" variant="h6">
         {title}:
       </Typography>
