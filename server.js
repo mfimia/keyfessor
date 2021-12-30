baa496f3baf694f5ef58bd27143650c7d7343c8d
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-30 13:57:13 +0100

    allow headers complying with CORS policy

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+// Avoiding CORS policy errors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 // Init Middleware
 app.use(express.json({ extended: false }));
 
