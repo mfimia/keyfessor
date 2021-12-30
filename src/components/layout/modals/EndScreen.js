817bb9db1cd4e3bfa92dcfa8aa0445a87c6e73bb
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-30 14:04:37 +0100

    accordion elements rendered on separate components

--- src/components/layout/modals/EndScreen.js
+++ src/components/layout/modals/EndScreen.js
@@ -1,42 +1,19 @@
 import { useContext } from "react";
 import MainContext from "../../../context/main/MainContext";
 import CalculateFinalScore from "../../utils/CalculateFinalScore";
-import { Box } from "@mui/system";
 import List from "@mui/material/List";
-import StarIcon from "@mui/icons-material/Star";
-import Rating from "@mui/material/Rating";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Typography, ListItemText, ListItem } from "@mui/material";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
-
-const iconStyle = {
-  fontSize: {
-    xs: "16px",
-    md: "20px",
-    lg: "22px",
-    xl: "28px",
-  },
-  color: "goldenrod",
-  p: 0,
-};
-
-const fontBreakpoints = {
-  xs: "14px",
-  md: "16px",
-  lg: "18px",
-  xl: "20px",
-};
-
-const titleBreakpoints = {
-  xs: "20px",
-  md: "22px",
-  lg: "24px",
-  xl: "28px",
-};
+import ResultItem from "./endTabs/ResultItem";
+import AdvancedStats from "./endTabs/AdvancedStats";
+import EndTitle from "./endTabs/EndTitle";
+import Leaderboard from "./endTabs/leaderboard/Leaderboard";
 
 const style = {
   position: "absolute",
@@ -46,8 +23,7 @@ const style = {
   width: {
     xs: "50%",
     md: "40%",
-    lg: "35%",
-    xl: "30%",
+    xl: "35%",
   },
   height: "90vh",
   bgcolor: "background.paper",
@@ -63,92 +39,10 @@ const EndScreen = () => {
   const { resetGame, laps, totalAppLetters } = mainContext;
 
   const finalScore = CalculateFinalScore();
-  const {
-    finalTotalTime,
-    averageAcc,
-    averageAccRating,
-    averageSpeed,
-    averageSpeedRating,
-    averageTime,
-    title,
-    overallRating,
-  } = finalScore;
 
-  const { scoreTitle, titleColor } = title;
-
-  const timeResults = laps.map((lap, index) => {
-    const { speed, accuracy, results, time } = lap;
-    const { speedResult, accuracyResult, overview } = results;
-    const { sentence, rating } = overview;
-
-    return (
-      <ListItem sx={{ p: 0, mb: 1 }} alignItems="flex-start" key={index}>
-        <ListItemText
-          primary={
-            <Typography component="div" sx={{ fontSize: fontBreakpoints }}>
-              <Typography
-                sx={{ fontSize: fontBreakpoints }}
-                component="span"
-                fontWeight={700}
-              >
-                Text {index + 1}
-              </Typography>{" "}
-              -{" "}
-              <Typography
-                sx={{ fontSize: fontBreakpoints, color: "text.primary" }}
-                component="span"
-              >
-                {Math.floor(time)} s
-              </Typography>{" "}
-              |{" "}
-              <Typography
-                sx={{ fontSize: fontBreakpoints, color: speedResult.color }}
-                component="span"
-              >
-                {speed} WPM
-              </Typography>{" "}
-              |{" "}
-              <Typography
-                sx={{ fontSize: fontBreakpoints, color: accuracyResult.color }}
-                component="span"
-              >
-                {accuracy}%
-              </Typography>
-            </Typography>
-          }
-          secondary={
-            <Typography component="div">
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <Box component="span" sx={{ mr: 1 }}>
-                  <Typography
-                    sx={{ fontSize: fontBreakpoints }}
-                    color={overview.color}
-                  >
-                    {sentence}
-                  </Typography>
-                </Box>
-                <Rating
-                  sx={iconStyle}
-                  precision={0.5}
-                  name="Result-overview"
-                  value={rating}
-                  readOnly
-                  emptyIcon={
-                    <StarIcon style={{ opacity: 0.1 }} sx={iconStyle} />
-                  }
-                />
-              </Box>
-            </Typography>
-          }
-        />
-      </ListItem>
-    );
-  });
+  const timeResults = laps.map((lap, index) => (
+    <ResultItem lap={lap} key={index} index={index} />
+  ));
 
   return (
     <Modal
@@ -157,38 +51,7 @@ const EndScreen = () => {
       aria-describedby="end-screen-modal-description"
     >
       <Box component="div" sx={style}>
-        <Typography
-          fontFamily="Righteous"
-          id="end-screen-modal-title"
-          variant="h3"
-          component="h3"
-        >
-          Great stuff!
-        </Typography>
-        <Typography
-          fontFamily="Righteous"
-          id="end-screen-modal-description"
-          sx={{ my: 2 }}
-        >
-          Your Keyfessor title is:
-        </Typography>
-        <Typography
-          sx={{ fontSize: titleBreakpoints }}
-          fontFamily="Righteous"
-          color={titleColor}
-          fontWeight={700}
-          letterSpacing={2}
-        >
-          {scoreTitle.toUpperCase()}
-        </Typography>
-        <Rating
-          sx={iconStyle}
-          precision={0.1}
-          name="Title-score"
-          value={overallRating}
-          readOnly
-          emptyIcon={<StarIcon style={{ opacity: 0.1 }} sx={iconStyle} />}
-        />
+        <EndTitle finalScore={finalScore} />
         <Box sx={{ my: 4 }}>
           <Accordion>
             <AccordionSummary
@@ -196,9 +59,7 @@ const EndScreen = () => {
               aria-controls="panel1a-content"
               id="Final-results"
             >
-              <Typography fontFamily="Righteous">
-                See detailed results
-              </Typography>
+              <Typography fontFamily="Righteous">Results</Typography>
             </AccordionSummary>
             <AccordionDetails>
               <List sx={{ pl: 2 }}>{timeResults}</List>
@@ -210,33 +71,30 @@ const EndScreen = () => {
               aria-controls="panel2a-content"
               id="Advanced-stats"
             >
-              <Typography fontFamily="Righteous">See advanced stats</Typography>
+              <Typography fontFamily="Righteous">Advanced Stats</Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography sx={{ px: 4, m: 2 }}>
-                You have typed a total of {totalAppLetters} characters in{" "}
-                {finalTotalTime} seconds
-              </Typography>
-              <Typography sx={{ mb: 2 }}>
-                Average time {averageTime} seconds per text
-              </Typography>
-              <Typography sx={{ mb: 2 }}>
-                Average speed {averageSpeed} WPM
-              </Typography>
-              <Typography sx={{ mb: 2 }}>
-                Average accuracy {averageAcc}%
-              </Typography>
-              <Typography sx={{ mb: 2 }}>
-                Average speed rating {averageSpeedRating}/10 points
-              </Typography>
-              <Typography sx={{ mb: 2 }}>
-                Average accuracy rating {averageAccRating}/10 points
-              </Typography>
+              <AdvancedStats
+                finalScore={finalScore}
+                totalAppLetters={totalAppLetters}
+              />
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3a-content"
+              id="Leaderboard"
+            >
+              <Typography fontFamily="Righteous">Leaderboard</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Leaderboard finalScore={finalScore} />
             </AccordionDetails>
           </Accordion>
         </Box>
         <Button sx={{ mt: 1 }} onClick={resetGame} size="large">
-          <Typography fontFamily="Righteous">Go again?</Typography>
+          <Typography fontFamily="Righteous">Restart</Typography>
         </Button>
       </Box>
     </Modal>
