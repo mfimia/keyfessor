3410f819dd07f69fe421757dd2dde0ff7e4edbb9
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-01 18:26:15 +0100

    stats mapped in table component

--- client/src/components/layout/modals/endTabs/AdvancedStats.js
+++ client/src/components/layout/modals/endTabs/AdvancedStats.js
@@ -1,5 +1,16 @@
-import { Fragment } from "react";
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import calculateStatsColors from "../../../utils/calculateStatsColors";
+
+const types = {
+  mb: 2,
+  fontFamily: "Ropa Sans",
+};
 
 const AdvancedStats = ({ finalScore, totalAppLetters }) => {
   const {
@@ -11,24 +22,83 @@ const AdvancedStats = ({ finalScore, totalAppLetters }) => {
     averageTime,
   } = finalScore;
 
+  const colors = calculateStatsColors(finalScore);
+
+  const { totalTime, avgTime, avgSpeed, avgAcc, avgSpeedRt, avgAccRt } = colors;
+
+  const averages = [
+    {
+      stat: "Time (per text)",
+      rating: averageTime,
+      unit: " s",
+      color: avgTime,
+    },
+    { stat: "Speed", rating: averageSpeed, unit: " WPM", color: avgSpeed },
+    { stat: "Accuracy", rating: averageAcc, unit: "%", color: avgAcc },
+    {
+      stat: "Speed Rating",
+      rating: averageSpeedRating,
+      unit: "/10",
+      color: avgSpeedRt,
+    },
+    {
+      stat: "Accuracy Rating",
+      rating: averageAccRating,
+      unit: "/10",
+      color: avgAccRt,
+    },
+  ];
+
   return (
-    <Fragment>
-      <Typography sx={{ px: 4, m: 2 }}>
-        You have typed a total of {totalAppLetters} characters in{" "}
-        {finalTotalTime} seconds
+    <Box component="div" textAlign="left">
+      <Typography sx={types}>
+        You have typed a total of{" "}
+        <Typography fontStyle="italic" fontFamily="Ropa Sans" component="span">
+          {totalAppLetters}{" "}
+        </Typography>
+        characters in{" "}
+        <Typography color={totalTime} fontFamily="Ropa Sans" component="span">
+          {finalTotalTime}
+        </Typography>{" "}
+        seconds
       </Typography>
-      <Typography sx={{ mb: 2 }}>
-        Average time {averageTime} seconds per text
-      </Typography>
-      <Typography sx={{ mb: 2 }}>Average speed {averageSpeed} WPM</Typography>
-      <Typography sx={{ mb: 2 }}>Average accuracy {averageAcc}%</Typography>
-      <Typography sx={{ mb: 2 }}>
-        Average speed rating {averageSpeedRating}/10 points
-      </Typography>
-      <Typography sx={{ mb: 2 }}>
-        Average accuracy rating {averageAccRating}/10 points
-      </Typography>
-    </Fragment>
+      <Box component="div">
+        <Typography
+          sx={{ my: 1 }}
+          textAlign="center"
+          component="h2"
+          variant="h6"
+        >
+          Averages
+        </Typography>
+      </Box>
+      <TableContainer
+        elevation={3}
+        sx={{ width: "60%", margin: "0 auto" }}
+        component={Paper}
+      >
+        <Table>
+          <TableBody>
+            {averages.map((average) => (
+              <TableRow
+                key={average.stat}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  <Typography fontFamily="Ropa Sans">{average.stat}</Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography color={average.color} fontFamily="Ropa Sans">
+                    {average.rating}
+                    {average.unit}
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 };
 
